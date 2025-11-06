55c56789a614d16f9b7ded799ffa7a01d8d75fbc
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-11-06 20:49:01 +0900

    client base

---
+++ packages/website/app/routes/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4 bg-gray-600 min-h-screen">
+      this is my zombie something game dashboard
+    </main>
+  );
+};
+
+export default Dashboard;
